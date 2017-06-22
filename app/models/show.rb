8c8e6b2b67a023d8c2bea5aefb772810c112c66f
Catherine <cmlavin7@gmail.com>
2017-06-22 12:17:19 -0400

    Done with Paul

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(network)
+    new_network = Network.new(network)
+    self.network = new_network
+  end
 end
